c48f709eda51009061441a3831403f4cb5c02ef8
Ekansh Saxena <ekansh005@gmail.com>
2021-10-05 14:25:39 +0300

    Fix responsive menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,19 @@
 const burger = document.querySelector("#burger");
 const menu = document.querySelector("#menu");
+const menu_close = document.querySelector("#menu-close");
 
-burger.addEventListener("click", (e) => {
+function toggleMenu(e) {
   if (menu.classList.contains("hidden")) {
     menu.classList.remove("hidden");
+    menu_close.classList.remove("hidden");
+    burger.classList.add("hidden");
   } else {
     menu.classList.add("hidden");
+    menu_close.classList.add("hidden");
+    burger.classList.remove("hidden");
   }
-});
+}
+
+burger.addEventListener("click", toggleMenu);
+
+menu_close.addEventListener("click", toggleMenu);
